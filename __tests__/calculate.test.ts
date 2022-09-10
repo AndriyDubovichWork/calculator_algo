9f0d442bbf7080b657bd95a258469d48cd2bccd9
AndriyDubovichWork <andriydubovichwork@gmail.com>
2022-09-10 17:28:09 +0300

    add some tests

--- __tests__/calculate.test.ts
+++ __tests__/calculate.test.ts
@@ -2,10 +2,26 @@ import { describe, expect, test } from '@jest/globals';
 import calculate from './../calculate';
 
 describe('calculate', () => {
-  test('has to operate', () => {
+  test('has to do single operations', () => {
     expect(calculate('2+2')).toBe(4);
     expect(calculate('4-2')).toBe(2);
     expect(calculate('4*2')).toBe(8);
     expect(calculate('8/2')).toBe(4);
+    expect(calculate('8^2')).toBe(64);
+    expect(calculate('2√25')).toBe(5);
+  });
+  test('has to do multiple operations', () => {
+    expect(calculate('2+2+2')).toBe(6);
+    expect(calculate('4-2-1')).toBe(1);
+    expect(calculate('4*2*2')).toBe(16);
+    expect(calculate('8/2/2')).toBe(2);
+    expect(calculate('2^2^2')).toBe(16);
+    expect(calculate('2√2√625')).toBe(5);
+  });
+  test('has to do hard operations', () => {
+    expect(calculate('2+2x2')).toBe(6);
+    expect(calculate('1+2*2/2')).toBe(3);
+    expect(calculate('2*10-2^3')).toBe(12);
+    expect(calculate('2√25-2^3')).toBe(-3);
   });
 });
