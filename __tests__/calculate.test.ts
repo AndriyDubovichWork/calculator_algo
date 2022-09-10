d99f97dc08f6bcd76aec734442c08fa4cb04ef7d
AndriyDubovichWork <andriydubovichwork@gmail.com>
2022-09-10 12:41:27 +0300

    fixed test issue

--- __tests__/calculate.test.ts
+++ __tests__/calculate.test.ts
@@ -4,5 +4,8 @@ import calculate from './../calculate';
 describe('calculate', () => {
   test('has to operate', () => {
     expect(calculate('2+2')).toBe(4);
+    expect(calculate('4-2')).toBe(2);
+    expect(calculate('4*2')).toBe(8);
+    expect(calculate('8/2')).toBe(4);
   });
 });
